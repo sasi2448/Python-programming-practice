0455235d3e3ce1ad536030835d9b1d7fe3f4dbbd
sasi2448 <sasisasichandra@gmail.com>
2018-07-04 10:33:42 +0530

    Create alphabetornot.py

---
+++ alphabetornot.py
@@ -0,0 +1,11 @@
+print"enter the an alphabet"
+a=raw_input()
+if len(a)==1:
+    if( a >=chr(65) and a<=chr(122)):
+        print"alphabet"
+    else :
+            print"its not a alphabet"
+
+            
+
+
