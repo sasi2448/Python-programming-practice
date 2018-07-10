7e120b214348b3e3b78300c5a9c2a5cb1c7d2fdd
sasi2448 <sasisasichandra@gmail.com>
2018-07-10 09:05:06 +0530

    Update arm.py

--- arm.py
+++ arm.py
@@ -1,11 +1,14 @@
-num = int(input())
+n = int(input())
 sum = 0
-temp = num
-while temp > 0:
-   digit = temp % 10
-   sum =sum+digit ** 3
-   temp=temp//10
-if num == sum:
-   print("yes")
+temp = n
+if(n<=100000):
+    while temp > 0:
+       digit = temp % 10
+       sum =sum+digit ** 3
+       temp=temp//10
+    if n == sum:
+       print("yes")
+    else:
+       print("no")
 else:
-   print("no")
+    print"invalid"
