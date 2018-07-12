3f408f55d6caf8fa1551845d70d7f33252bc9ab3
sasi2448 <sasisasichandra@gmail.com>
2018-07-12 22:39:16 +0530

    Update alphabet.py

--- alphabet.py
+++ alphabet.py
@@ -1,7 +1,10 @@
-print"enter the an alphabet"
+
 a=raw_input()
 if len(a)==1:
     if( a >=chr(65) and a<=chr(122)):
         print"yes"
     else :
-            print"no"
+            print"No"
+else:
+    print"No"
+
