25a908423ccd09c5ee1f7a7f605c55348938eb81
sasi2448 <sasisasichandra@gmail.com>
2018-07-12 22:34:02 +0530

    Update vowelandconsonants.py

--- vowelandconsonants.py
+++ vowelandconsonants.py
@@ -3,9 +3,9 @@ a=raw_input()
 list={'a','e','i','o','u','A','E','I','O','U'}
 if len(a)==1:
     if a in list:
-        print "vowel"
+        print "Vowel"
     elif( a >=chr(65) and a<=chr(122)):
-        print"consonant"
+        print"Consonant"
     else :
             print"invalid"
 else:
