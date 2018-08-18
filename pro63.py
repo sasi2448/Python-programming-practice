98390a23579822962644d94c5867ab10a958ac7b
sasi2448 <sasisasichandra@gmail.com>
2018-08-18 16:42:10 +0530

    Update pro63.py

--- pro63.py
+++ pro63.py
@@ -2,23 +2,29 @@ a=input()
 b=list(a)
 d=[]
 q=[]
+s=list(set(a))
+s.sort()
 count=0
-for i in range(len(b)):
-    c=ord(b[i])
-    d.append(c)
+if(b!=s):
+    for i in range(len(b)):
+        c=ord(b[i])
+        d.append(c)
 
-m=min(d)
-for j in range(len(b)):
-        q.append(m)
-        m=m+1
+    m=min(d)
+    for j in range(len(b)):
+            q.append(m)
+            m=m+1
 
-d=list(d)
-q=list(q)
+    d=list(d)
+    q=list(q)
+   
 
-
-for i in range(0,len(q)):
-        if(q[i]==d[i]):
-            count+=1
-        else:
-            break
+    for i in range(0,len(q)):
+            if(q[i]==d[i]):
+                count+=1
+            else:
+                break
+else:
+    count=count+len(b)
 print(count)
+
